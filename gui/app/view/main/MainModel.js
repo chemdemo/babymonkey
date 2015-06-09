6490bb2464ca948b599242f05fb947482adbc15f
chemdemo <yangdemo@gmail.com>
2015-06-09 18:22:07 +0800

    init sencha proj

---
+++ gui/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('babymonkey.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'babymonkey'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
